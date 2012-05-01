a7f76e2d2ff24b1ac065fbe17542c08251dd9197
Daniel Hackney <dan@haxney.org>
2012-04-30 21:37:00 -0400

    Start building a standalone tester for p9 regex

---
+++ p9regtest/p9regexp.c
@@ -0,0 +1,29 @@
+/**
+ * Runner for plan9 regexp library
+ */
+
+#include "lib9.h"
+#include <regexp9.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+
+        Reprog *compiled = regcomp(argv[1]);
+        int place = 2;
+        char *line;
+        size_t len = 0;
+        ssize_t read_length;
+
+        while ((read_length = getline(&line, &len, stdin)) != -1) {
+                if (regexec(compiled, line, 0, 0) == 1) {
+                        fprintf(stdout, "%s\n", line);
+                }("%s", line);
+        }
+
+        exit(0);
+}
+
+/* Local Variables: */
+/* c-basic-offset: 8 */
+/* End: */
